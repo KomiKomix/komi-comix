59af8fc21d150224e35709994efac76743041cbb
«Evgeny <caldariset@gmail.com>
2015-12-11 08:54:59 +0400

    customize spree

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -11,43 +11,33 @@
 # In order to initialize a setting do:
 # config.setting_name = 'new value'
 
-Spree::AppConfiguration.class_eval do
-  #preference :facebook_app_id, :string
-  #preference :facebook_layout, :string
-  #preference :order_emails_bcc, :text
-  #preference :delivery_time_in_days, :number
-  #preference :use_common_meta, :boolean, default: false
-  #preference :common_meta_title, :string, default: '{{name}} za {{price}} złotych {{#properties}}{{Model}}{{/properties}} '
-  #preference :common_meta_description, :string, default: '{{name}} ({{#properties}}{{Model}}{{/properties}} ) z kategorii: {{taxon}}. Dostępny w cenie: {{price}} złotych. '
 
-  #preference :taxon_use_common_meta, :boolean, default: false
-  #preference :taxon_common_meta_title, :string, default: "{{name}} - {{parent_name}}"
-  #preference :taxon_common_meta_description, :string, default: "Ilość produktów: {{products_count}}. Kategoria: {{name}} {{parent_name}}. Ostatnio dodane produkty: {{product3_name}}, {{product2_name}}, {{product1_name}}."
-end
 
 Spree.config do |config|
   # Example:
   # Uncomment to stop tracking inventory levels in the application
   # config.track_inventory_levels = false
+
   #config.allow_ssl_in_staging = true
   #config.allow_ssl_in_production = true
   config.logo = 'logo/favicon.ico'
-  #config.layout = 'application'
+  config.layout = 'application'
   #config.default_country_id = 72
-  config.products_per_page = 48
+  config.products_per_page = 16
   config.allow_guest_checkout = true
   config.always_include_confirm_step = true
   #config.mails_from = ""
   #config.facebook_app_id = '844425155590323'
   #config.facebook_layout = 'button_count'
-  #config.order_emails_bcc = 'caldariset@gmail.com, sklep.internetowy@yes.pl, zloto.inwestycyjne@yes.pl'
+  #config.order_emails_bcc = ''
   #config.delivery_time_in_days = 3
   #config.searcher_class = Spree::Search::YgiSearch
   #config.use_common_meta = true
   #config.taxon_use_common_meta = true
 
+
   #S3 configuration
-  if Rails.env.production? then
+  if Rails.env.production?
     #production. Store images on S3.
     # development will default to local storage
 
