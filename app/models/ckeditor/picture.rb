6731fab51ee0d22a5bd5f5a4d5e92cbd111cceb3
«Evgeny <caldariset@gmail.com>
2015-12-20 17:17:36 +0400

    fix ckeditor s3 settings

--- app/models/ckeditor/picture.rb
+++ app/models/ckeditor/picture.rb
@@ -1,14 +1,16 @@
 class Ckeditor::Picture < Ckeditor::Asset
   has_attached_file :data,
                     storage: :s3,
+                    s3_protocol: :https,
                     s3_credentials: {
                       access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
-                      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-                      bucket:            ENV['S3_BUCKET'],
-                      region: 'us-east-1'
+                      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
                     },
-                    url: 'ckeditor_assets/pictures/:id/:style_:basename.:extension',
-                    path: 'ckeditor_assets/pictures/:id/:style_:basename.:extension'
+                    bucket: ENV['S3_BUCKET'],
+                    region: 'us-west-2',
+                    url: ":s3_alias_url",
+                    s3_host_alias: "s3-us-west-2.amazonaws.com/komicomix",
+                    path: 'ckeditor_assets/pictures/:id/original_:basename.:extension'
 
   validates_attachment_presence :data
   validates_attachment_size :data, less_than: 2.megabytes
