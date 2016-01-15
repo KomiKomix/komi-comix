1f84b81f9a853422dc1e025d3c4a746818a33952
«Evgeny <caldariset@gmail.com>
2016-01-15 21:16:22 +0400

    small fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -32,8 +32,8 @@ set :branch,        :master
 # set :keep_releases, 5
 
 ## Linked Files & Directories (Default None):
- set :linked_files, %w{config/database.yml config/secrets.yml}
-# set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_files, %w{config/database.yml config/secrets.yml}
+set :linked_dirs,  %w{public/spree public/ckeditor_assets}
 
 set :rake, "bundle exec rake"
 
