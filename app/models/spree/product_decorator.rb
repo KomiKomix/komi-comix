71c763cb4aad4f1e9381bfe5a28e18665b474550
Olga Chuprova <olga.chuprova@gmail.com>
2016-11-24 13:48:37 +0300

    Add current date to product init form field "available_on"

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -2,6 +2,13 @@ Spree::Product.class_eval do
   translates :caption
   scope :rand, -> { order('RANDOM()').limit(3) }
 
+  before_save :set_position
+
+  def initialize(*args)
+    super(*args)
+    self.available_on = DateTime.current.strftime('%d.%m.%Y')
+  end
+
   add_search_scope :ascend_by_caption_and_name do
     order([caption: :asc]) #, name: :asc])
   end
