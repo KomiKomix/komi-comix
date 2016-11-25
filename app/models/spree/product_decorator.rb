b7742021c33f99f58cf9ac30880544f8b24c6ebc
Olga Chuprova <olga.chuprova@gmail.com>
2016-11-25 16:42:16 +0300

    Default shipping category for product

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -7,6 +7,8 @@ Spree::Product.class_eval do
   def initialize(*args)
     super(*args)
     self.available_on = DateTime.current.strftime('%d.%m.%Y')
+    # self.option_types =
+    self.shipping_category = Spree::ShippingCategory.first
   end
 
   add_search_scope :ascend_by_caption_and_name do
