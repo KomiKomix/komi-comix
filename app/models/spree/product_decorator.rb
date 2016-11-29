1cb30ea2e53543dab36716ed0552ecf7b0dae10f
Olga Chuprova <olga.chuprova@gmail.com>
2016-11-29 19:32:49 +0300

    Add default shipping category and prototype in new product form

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -7,8 +7,11 @@ Spree::Product.class_eval do
   def initialize(*args)
     super(*args)
     self.available_on = DateTime.current.strftime('%d.%m.%Y')
-    # self.option_types =
     self.shipping_category = Spree::ShippingCategory.first
+    prototype = Spree::Prototype.first
+    if prototype
+      self.prototype_id = prototype.id
+    end
   end
 
   add_search_scope :ascend_by_caption_and_name do
