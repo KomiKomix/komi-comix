e4055b4534ab091048a203a57953c088e685647c
Olga Chuprova <olga.chuprova@gmail.com>
2016-11-30 20:12:18 +0300

    Show products in admin descending by creation date

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -2,6 +2,8 @@ Spree::Product.class_eval do
   translates :caption
   scope :rand, -> { order('RANDOM()').limit(3) }
 
+  self.whitelisted_ransackable_attributes = %w[slug caption created_at]
+
   add_search_scope :ascend_by_caption_and_name do
     order([caption: :asc]) #, name: :asc])
   end
