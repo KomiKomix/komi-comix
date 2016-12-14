b724f275e07a6f963e955d27f7abf4a6cff23cb2
Chikiro <mail@neko-sama.ru>
2016-12-14 19:42:25 +0300

    remove lightbox init

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,9 +19,6 @@
 //= require_tree ./lightbox
 
 $(function() {
-  $(document).ready(function(){
-    lightbox.init();
-  });
 
   $(document).on("click", "#products_sort a, #products_row .pagination a", function() {
     $.getScript(this.href);
