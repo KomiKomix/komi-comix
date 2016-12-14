37c48262bb5edf88b853904fc7096f7a171450ef
Chikiro <mail@neko-sama.ru>
2016-12-14 15:48:38 +0300

    fix invalid html in news

--- app/models/spree/post_decorator.rb
+++ app/models/spree/post_decorator.rb
@@ -1,3 +1,12 @@
 Spree::Post.class_eval do
   scope :published, -> { where(published: true).order(created_at: 'desc') }
+
+  before_save :fix_invalid_html
+
+  def fix_invalid_html
+    if self.description?
+      self.description = Nokogiri::HTML::DocumentFragment.parse(self.description).to_html
+    end
+  end
+
 end
