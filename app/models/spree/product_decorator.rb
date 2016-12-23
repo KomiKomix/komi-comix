a0baab34eb27687d9e9d19b15336baabf4204c1b
Chikiro <mail@neko-sama.ru>
2016-12-23 18:22:59 +0300

    swap caption and name

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -12,6 +12,10 @@ Spree::Product.class_eval do
     end
   end
 
+  def name_with_caption
+    "#{self.name} #{self.caption}".strip
+  end
+
   self.whitelisted_ransackable_attributes = %w[slug caption created_at]
 
   add_search_scope :ascend_by_caption_and_name do
